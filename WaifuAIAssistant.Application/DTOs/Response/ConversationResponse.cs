d56eabbbfadb2f9faeaf2403b058b0b9aa8ecc6d
tungske <trinhsontung2410@gmail.com>
2026-06-12 15:09:03 +0700

    feat: add avatar URL into response

--- WaifuAIAssistant.Application/DTOs/Response/ConversationResponse.cs
+++ WaifuAIAssistant.Application/DTOs/Response/ConversationResponse.cs
@@ -11,6 +11,7 @@ namespace WaifuAIAssistant.Application.DTOs.Response
         public int Id { get; set; }
         public int? ModelCharacterId { get; set; }
         public string Title { get; set; } = string.Empty;
+        public string AvatarUrl { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
